fd9e49b71c50bd0ee63578e3750bfd10309e0ff3
Damien Burke <damien@damienburke.com>
2015-06-30 09:55:31 -0700

    Make spacing consistent

--- exercism-swift/WordCount.swift
+++ exercism-swift/WordCount.swift
@@ -9,33 +9,31 @@
 import Foundation
 
 class WordCount {
-    let inputString:String
-    
-    init(words:String){
-        inputString = words
+  let inputString:String
+  
+  init(words:String){
+    inputString = words
+  }
+  
+  func count() -> Dictionary<String,Int> {
+    var runningCount = [String:Int]()
+    var wordList = getWordList(inputString)
+  
+    for word in wordList {
+      if let previousCount = runningCount[word] {
+        runningCount[word] = previousCount + 1
+      } else {
+        runningCount[word] = 1
+      }
     }
+    return runningCount
+  }
     
-    func count() -> Dictionary<String,Int> {
-        var runningCount = [String:Int]()
-        
-        var wordList = getWordList(inputString)
-      
-        for word in wordList {
-            if let previousCount = runningCount[word] {
-                runningCount[word] = previousCount + 1
-            } else {
-                runningCount[word] = 1
-            }
-        }
-        
-        return runningCount
-    }
-    
-    private func getWordList(input:String) -> Array<String> {
-      let lowercased_input = input.lowercaseString
-      let unwantedChars = NSCharacterSet.alphanumericCharacterSet().invertedSet
-      var words = lowercased_input.componentsSeparatedByCharactersInSet(unwantedChars)
-      words = words.filter({$0 != ""})
-      return words
-    }
+  private func getWordList(input:String) -> Array<String> {
+    let lowercased_input = input.lowercaseString
+    let unwantedChars = NSCharacterSet.alphanumericCharacterSet().invertedSet
+    var words = lowercased_input.componentsSeparatedByCharactersInSet(unwantedChars)
+    words = words.filter({$0 != ""})
+    return words
+  }
 }
